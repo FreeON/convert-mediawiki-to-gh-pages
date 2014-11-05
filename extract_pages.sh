e84820c4c6fac6dd2386e6bbebe8fb62b7de5b61
Nicolas Bock <nicolasbock@gmail.com>
2014-11-05 11:25:42 -0700

    Exclude Talk pages.

--- extract_pages.sh
+++ extract_pages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#
+# vim: tw=0
 
-echo "USE freeon_wiki; SELECT page.page_title, text.old_text FROM text INNER JOIN revision ON revision.rev_text_id = text.old_id INNER JOIN page ON page.page_id = revision.rev_page WHERE revision.rev_id = page.page_latest" | mysql -u wikiuser -p > pages.txt
+echo "USE freeon_wiki; SELECT page.page_title, text.old_text FROM text INNER JOIN revision ON revision.rev_text_id = text.old_id INNER JOIN page ON page.page_id = revision.rev_page WHERE (revision.rev_id = page.page_latest && page.page_namespace = 0)" | mysql -u wikiuser -p > pages.txt
